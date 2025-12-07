6afa1eb2897e496e64895ed31f50b74d6197b698
Smoothzada <brunolimarjory@gmail.com>
2025-12-07 07:58:26 -0300

    Delete PowershellScripts/Scheduler1.ps1

--- PowershellScripts/Scheduler1.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'SilentlyContinue';
-Get-ChildItem -Path C:\Windows\System32\Tasks\ -Recurse | ForEach-Object {
-                        Select-String -Path $_.FullName -Pattern ":\\" 
-                    } | Out-Gridview
