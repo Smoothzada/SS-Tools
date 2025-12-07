b5ac0ce8e86cf197a13cb6c2065b53d94154a5d2
Smoothzada <brunolimarjory@gmail.com>
2025-12-07 07:47:59 -0300

    Create Scheduler1.ps1

---
+++ PowershellScripts/Scheduler1.ps1
@@ -0,0 +1,3 @@
+Get-ChildItem -Path C:\Windows\System32\Tasks\ -Recurse | ForEach-Object {
+                        Select-String -Path $_.FullName -Pattern ":\\" 
+                    } | Out-Gridview
